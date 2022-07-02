9f90d98ad4887d52c5a32fea6ed998c774b48a9e
AidanGamble <aga99@sfu.ca>
2022-07-02 12:40:04 -0700

    Caretaker Login/Register Pages recreated

---
+++ app/src/main/java/com/example/storbook/CaretakerRegister.java
@@ -0,0 +1,14 @@
+package com.example.storbook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CaretakerRegister extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_caretaker_register);
+    }
+}
