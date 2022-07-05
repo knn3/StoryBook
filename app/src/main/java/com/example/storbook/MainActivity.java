823319f2294f64c91d4d868d82d8432493028f9d
Nguyên Nguyễn <khoi.nguyen.8602@gmail.com>
2022-07-05 13:09:17 -0700

    finished uploading from caretaker side

--- app/src/main/java/com/example/storbook/MainActivity.java
+++ app/src/main/java/com/example/storbook/MainActivity.java
@@ -1,4 +1,5 @@
 package com.example.storbook;
+import android.content.Intent;
 import android.text.Html;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -6,7 +7,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.text.method.LinkMovementMethod;
 import android.text.util.Linkify;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
+
+import com.example.storbook.databinding.ActivityMainBinding;
+import com.google.firebase.functions.FirebaseFunctions;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -18,13 +25,40 @@ public class MainActivity extends AppCompatActivity {
         CharSequence web= "create new account";
         t1.setText(web);
         t1.setMovementMethod(LinkMovementMethod.getInstance());
+        // 10.0.2.2 is the special IP address to connect to the 'localhost' of
+        // the host computer from an Android emulator.
+        FirebaseFunctions functions = FirebaseFunctions.getInstance();
+        functions.useEmulator("10.0.2.2", 5001);
+
+//        imgButton = (ImageButton) findViewById(R.id.btnct);
+//        imgButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                openCaretakerActivity();
+//            }
+//        });
+
+
+
+
+
+//        ImageButton button = findViewById(R.id.btnct);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                    Intent intent = new Intent(MainActivity.this, Caretaker.class);
+//                    startActivity(intent);
+//            }
+//        });
 
     }
     public void onpwdClicked(View view){
 
     }
-    public void onctClicked(View view){
 
+    public void onctClicked( View view){
+        Intent intent = new Intent(MainActivity.this, Caretaker.class);
+        startActivity(intent);
     }
 
 }
