cb9de2b4e0eb8ede2cb01bcaad620960a39cac8a
BillyShi <lsa84@sfu.ca>
2022-07-26 13:37:06 -0700

    Used a new starting activity to initialize all data without reloading things when rerouting back to main activity

---
+++ app/src/main/java/com/example/storbook/InitializeActivity.java
@@ -0,0 +1,112 @@
+package com.example.storbook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class InitializeActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        // Jump to login page if did not log in
+        if(currentUser == null){
+            Intent i = new Intent(InitializeActivity.this, CaretakerLogin.class);
+            startActivity(i);
+        }
+        // So already logged in go the the main page
+        else{
+            Intent i = new Intent(InitializeActivity.this, MainActivity.class);
+            startActivity(i);
+        }
+        // Refresh the local FM list if have internet access
+        if (isNetworkAvailable()) {
+            ((global) this.getApplication()).refreshFMlist();
+            Toast.makeText(getApplicationContext(), "Synced with cloud", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            ((global) this.getApplication()).refreshFMlist();
+            Toast.makeText(getApplicationContext(), "No internet, Entering offline mode!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        // Jump to login page if did not log in
+        if(currentUser == null){
+            Intent i = new Intent(InitializeActivity.this, CaretakerLogin.class);
+            startActivity(i);
+        }
+        // So already logged in go the the main page
+        else{
+            Intent i = new Intent(InitializeActivity.this, MainActivity.class);
+            startActivity(i);
+        }
+    }
+
+    public boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null;
+    }
+
+    public boolean isInternetWorking() {
+        boolean success = false;
+        try {
+            URL url = new URL("https://google.com");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setConnectTimeout(10000);
+            connection.connect();
+            success = connection.getResponseCode() == 200;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return success;
+    }
+
+    public boolean hasInternetAccess() {
+        if (isNetworkAvailable()) {
+            try {
+                HttpURLConnection urlc = (HttpURLConnection)
+                        (new URL("http://clients3.google.com/generate_204")
+                                .openConnection());
+                urlc.setRequestProperty("User-Agent", "Android");
+                urlc.setRequestProperty("Connection", "close");
+                urlc.setConnectTimeout(1500);
+                urlc.connect();
+                return (urlc.getResponseCode() == 204 &&
+                        urlc.getContentLength() == 0);
+            } catch (IOException e) {
+                Toast.makeText(getApplicationContext(), "Error Checking Internet Status", Toast.LENGTH_SHORT).show();
+            }
+        }
+        else {
+            Toast.makeText(getApplicationContext(), "No internet, Entering offline mode!", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return false;
+    }
+}
